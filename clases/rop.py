a8dd58d8a6f64b10b567ea0e9a3b3cd87618d8fd
willigm <58922780+willigm@users.noreply.github.com>
2022-04-22 15:43:51 -0600

    agregando punto de reorden con desviacion estandar
    by willipro

--- clases/rop.py
+++ clases/rop.py
@@ -15,3 +15,20 @@ class ROP:
 
 rop_p = ROP(1500000,360,2)
 ROP=rop_p.calcular_rop()
+
+#punto de reorden con desviacion estandar
+
+class ROP_ESTANDAR:
+    def __init__(self,demanda_por_dia,plazo_entrega,z,desviacion):
+        self.demanda_por_dia= demanda_por_dia
+        self.plazo_entrega= plazo_entrega
+        self.desviacion=desviacion
+
+    def calcular_desviacion(self):
+        Rl= self.desviacion*sqrt(self.plazo_entrega)
+        rop_estandar= float(self.demanda_por_dia*self.plazo_entrega)+(self.z*Rl)
+        return rop_estandar
+#prueba x2
+
+rop_estandar=ROP_ESTANDAR(200,4,1.65,150)
+ROP_ESTANDAR= rop_estandar.calcular_desviacion()
